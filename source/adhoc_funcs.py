bac583b7f9f324efbc5b62f88152fb4ae83d88ca
tarikcurto <centro.tarik@live.com>
2024-07-17 14:43:45 +0200

    added ctx

--- source/adhoc_funcs.py
+++ source/adhoc_funcs.py
@@ -1,5 +1,8 @@
-def parse_noproxy(value):
-    return ','.join(value)
+def parse_noproxy(value, ctx):
+    local_proxy = ctx.get('local_value') or []
+    global_proxy = ctx.get('global_value') or []
+    proxy = local_proxy + global_proxy
+    return ','.join(proxy)
 
 adhoc_funcs = {
     'noProxy': parse_noproxy 
